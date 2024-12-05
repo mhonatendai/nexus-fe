8ffa67bbb82a8693ae57cc1d7420d0058036e226
Tendai E. Mhona <mhonatendai@gmail.com>
2024-12-04 22:54:47 -0500

    Refactoring the login page and fixing regression

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import Login from './Welcome'
+import Login from './Login'
 function App() {
   return (
     <Router>
