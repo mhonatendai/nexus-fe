0e780738605483707aa4bed8ed826785176c3cb6
Tendai E. Mhona <mhonatendai@gmail.com>
2024-12-28 21:36:40 -0500

    WIP - login page.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import LoginForm from "./pages/login/LoginForm";
 
 const App = () => {
   return (
     <Router>
-      <div className="App">
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
+        <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
     </Router>
   );
 };
