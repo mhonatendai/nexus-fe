4d1c1ba6398be85ac177e68fb7e27a7471441974
Tendai E. Mhona <mhonatendai@gmail.com>
2024-12-29 23:35:43 -0500

    Login updates, to be optimized.

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -2,23 +2,57 @@ import React  from "react";
 import './LoginForm.css';
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
+import { useState } from 'react';
+import axios from 'axios';
+
 
 const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        try {
+        console.log("Username: {}",username);
+
+        //   const response = await axios.post('http://localhost:8097/login', {
+        //     username: username,
+        //     password: password
+        //   });
+    
+        //   console.log(response.data);
+        } catch (error) {
+          // Handle errors
+          console.error('Error submitting form:', error);
+        }
+      };
+
     return (
         <div className='wrapper'>
             <form action="">
                 <h1>Sign in</h1>
                 <div className="input-box">
-                    <input type="text" placeholder='Username' required/>
+
+                    <input
+                     type="text" placeholder='Username' required
+                     value={username} onChange={(e) => setUsername(e.target.value)}
+                     />
                     <FaUser className="icon"/>
+
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder='Password' required/>
+                    
+                    <input
+                    type="password" placeholder='Password' required
+                    value={password} onChange={(e) => setPassword(e.target.value)}
+                    />
                     <FaLock className="icon"/>
+
                 </div>
                 <button type="submit">Login</button>
                 <div className="register-link">
-                    <p>New employee? <a href="#">Register</a></p>
+                    <p>Joining? <a href="#">Register</a></p>
                 </div>
             </form>
         </div>
