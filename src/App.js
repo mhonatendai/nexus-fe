9a4caa73bef76b29cb1a4e6cdbc6aa79e253a5ae
Tendai E. Mhona <mhonatendai@gmail.com>
2024-12-31 18:44:01 -0500

    Adding registration route.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import LoginForm from "./pages/login/LoginForm";
+import RegisterForm from './pages/register/RegisterForm';
 
 const App = () => {
   return (
     <Router>
         <Routes>
             <Route path="/" element={<LoginForm />} />
+            <Route path="/register" element={<RegisterForm />} />
             <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
     </Router>
